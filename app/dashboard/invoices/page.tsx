253d9e34e076371765989d9194e9622794e5df35
欧洲海豹 <zchcewd@gmail.com>
2024-04-30 16:24:00 +0800

    updata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,29 @@
-export default function Page() {
+import Search from '@/app/ui/search';
+import {CreateInvoice} from '@/app/ui/invoices/buttons';
+import {lusitana} from '@/app/ui/fonts';
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
+import {Suspense} from "react";
+import InvoicesTable from "@/app/ui/invoices/table";
+
+
+export default async function Page({searchParams}: { searchParams: { query: string, currentPage: number } }) {
+    const {query, currentPage} = searchParams;
+
     return (
-        <>Invoices Page</>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..."/>
+                <CreateInvoice/>
+            </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+                <InvoicesTable query={query} currentPage={currentPage}/>
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                {/* <Pagination totalPages={totalPages} /> */}
+            </div>
+        </div>
     );
 }
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,36 +1,39 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+import {BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon,} from '@heroicons/react/24/outline';
+import {fetchCardData} from "@/app/lib/data";
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
-  collected: BanknotesIcon,
-  customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+    collected: BanknotesIcon,
+    customers: UserGroupIcon,
+    pending: ClockIcon,
+    invoices: InboxIcon,
 };
 
-/*export default async function CardWrapper() {
-  return (
-    <>
-      {/!* NOTE: comment in this code when you get to this point in the course *!/}
+export default async function CardWrapper() {
+    const {
+        numberOfCustomers,
+        numberOfInvoices,
+        totalPaidInvoices,
+        totalPendingInvoices
+    } = await fetchCardData();
+    
+    return (
+        <>
+            {/* NOTE: comment in this code when you get to this point in the course */}
 
-       <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> 
-    </>
-  );
-}*/
+            <Card title="Collected" value={totalPaidInvoices} type="collected"/>
+            <Card title="Pending" value={totalPendingInvoices} type="pending"/>
+            <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
+            <Card
+                title="Total Customers"
+                value={numberOfCustomers}
+                type="customers"
+            />
+        </>
+    );
+}
 
-export default function Card({
+function Card({
   title,
   value,
   type,
